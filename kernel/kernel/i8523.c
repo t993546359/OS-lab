2d2ebb969305d3d773bdd06d129af6d2cb07eaf1
t993546359 <993546359@qq.com>
2018-05-03 15:20:19 +0800

    lab

---
+++ kernel/kernel/i8523.c
@@ -0,0 +1,11 @@
+#define TIMER_PORT 0x40
+#define FREQ_8253 1193182
+#define HZ 100
+
+void initTimer()
+{
+    int counter = FREQ_8253 / HZ;
+    outByte(TIMER_PORT + 3, 0x34);
+    outByte(TIMER_PORT, counter % 256 );
+    outByte(TIMER_PORT, counter / 256);
+}
